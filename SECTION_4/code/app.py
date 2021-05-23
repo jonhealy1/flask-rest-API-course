f07055f2436f541e49590c8ca0b7a8a5b3447028
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-23 16:31:59 -0700

    add lambda function to return items

--- SECTION_4/code/app.py
+++ SECTION_4/code/app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app=Flask(__name__)
 api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # for item in items:
+        #     if item["name"] == name:
+        #         return item
+        return {"item": item}, 404
+
+    def post(self, name):
+        data = request.get_json(force=True)
+        item = {"name": name, "price": data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
+app.run(port=5000, debug=True)
 
