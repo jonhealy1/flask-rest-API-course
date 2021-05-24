f681e67e2ca82fe357c250eb102199f63a097be4
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-24 12:49:08 -0700

    authenticate users

--- SECTION_4/code/security.py
+++ SECTION_4/code/security.py
@@ -1,26 +1,23 @@
+from user import User
+from werkzeug.security import safe_str_cmp
+
 users = [
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
+    User(1, 'bob', 'asdf')
 ]
 
-username_mapping = { 'bob': {
-    'id': 1,
-    'username': 'bob',
-    'password': 'asdf'
-}}
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id: u for u in users}
 
-userid_mapping = { 1: {
-    'id': 1,
-    'username': 'bob',
-    'password': 'asdf'
-}}
+
+# userid_mapping = { 1: {
+#     'id': 1,
+#     'username': 'bob',
+#     'password': 'asdf'
+# }}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
